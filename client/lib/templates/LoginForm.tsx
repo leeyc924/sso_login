edb977bbbe148d29e94b55e6051960aef6f101bd
leeyc924 <leeyc924@naver.com>
2023-01-03 01:01:08 +0900

    add alias import

---
+++ client/lib/templates/LoginForm.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@components";
+import React from "react";
+import styled from "styled-components";
+
+const LoginForm = () => {
+  return (
+    <Component>
+      <Button>
+        
+      </Button>
+    </Component>
+  );
+};
+
+const Component = styled.div``;
+
+export default React.memo(LoginForm);
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { Button } from "@components";
 
 function Home() {
   return (
@@ -9,11 +10,8 @@ function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        hello world
-      </div>
     </>
-  )
+  );
 }
 
 export default Home;
